88285e8c619fc77f319ab682a616759c4944fb6b
Jean Souza <jeanhsouza@gmail.com>
2022-11-04 13:20:50 -0300

    fix/dropdown

--- src/scripts/dropdown.js
+++ src/scripts/dropdown.js
@@ -3,7 +3,7 @@ export function dropdown(){
     const boxHamb = document.querySelector(".boxHamb")
     const navBoxHeader = document.querySelector(".navBoxHeader")
     boxHamb.addEventListener("click", ()=>{
-        if(boxHamb.src == "http://127.0.0.1:5500/src/assets/img/img_hamb.svg"){
+        if(navBoxHeader.classList.contains("none")){
             navBoxHeader.classList.remove("none")
             navBoxHeader.classList.add("flex"); 
             boxHamb.src = "../../assets/img/img_close.svg"        
